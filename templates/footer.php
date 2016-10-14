25b63d250b0686a2dda03b540500a56281afdcb3
macmarcelo <mac@follow.com.au>
2016-10-14 12:59:35 +0800

    Initial Update
    Initial Update

---
+++ templates/footer.php
@@ -0,0 +1,45 @@
+<footer class="content-info">
+  <div class="container">
+    <?php //dynamic_sidebar('sidebar-footer'); ?>
+    <div class="row">
+    	<div class="col-xs-12 col-sm-4">
+    		<div class="widget">
+    			<h3 class="widget-title text-uppercase">Widget 1</h3>
+    			<ul>
+    				<li><a href="#">Item List 1</a></li>
+    				<li><a href="#">Item List 2</a></li>
+    				<li><a href="#">Item List 3</a></li>
+    				<li><a href="#">Item List 4</a></li>
+    				<li><a href="#">Item List 5</a></li>
+    			</ul>
+    		</div>
+    	</div>
+
+    	<div class="col-xs-12 col-sm-4">
+    		<div class="widget">
+    			<h3 class="widget-title">Widget 1</h3>
+    			<ul>
+    				<li><a href="#">Item List 1</a></li>
+    				<li><a href="#">Item List 2</a></li>
+    				<li><a href="#">Item List 3</a></li>
+    				<li><a href="#">Item List 4</a></li>
+    				<li><a href="#">Item List 5</a></li>
+    			</ul>
+    		</div>
+    	</div>
+
+    	<div class="col-xs-12 col-sm-4">
+    		<div class="widget">
+    			<h3 class="widget-title">Widget 1</h3>
+    			<ul>
+    				<li><a href="#">Item List 1</a></li>
+    				<li><a href="#">Item List 2</a></li>
+    				<li><a href="#">Item List 3</a></li>
+    				<li><a href="#">Item List 4</a></li>
+    				<li><a href="#">Item List 5</a></li>
+    			</ul>
+    		</div>
+    	</div>
+    </div>
+  </div>
+</footer>
